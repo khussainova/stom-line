2ab323dd5b52dbc775cda661e8decabe4099f6ab
Karina Khussainova <khussainova.kr@gmail.com>
2022-08-30 21:37:58 +0600

    debugging

--- index.php
+++ index.php
@@ -1,23 +1,7 @@
 <?php
 
 require_once 'conn.php';
-$conn = mysqli_connect($_ENV['HOST'], $_ENV['USER'], $_ENV['PASS'], $_ENV['DB']);
 
-if (isset($_POST['submit'])) {
-
-    $name = mysqli_real_escape_string($conn, $_POST['name']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $number = $_POST['number'];
-    $date = $_POST['date'];
-
-    $insert = mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, date) VALUES('$name', '$email', '$number', '$date')") or die('query failed');
-
-    if ($insert) {
-        $message[] = 'appointment made successfully!';
-    } else {
-        $message[] = 'appointment failed';
-    }
-}
 
 ?>
 
