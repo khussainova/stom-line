35c313c8e6f3b57ac6a01a525bcdf381c984e9e9
Karina Khussainova <khussainova.kr@gmail.com>
2022-09-01 07:15:40 +0600

    debugging

--- conn.php
+++ conn.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $host = $_ENV['HOST'];
 $db = $_ENV['DB'];
 $user = $_ENV['USER'];
